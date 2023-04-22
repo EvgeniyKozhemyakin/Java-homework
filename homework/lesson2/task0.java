07c9349213de3a3bdea3073b17d9d2795e3dde8a
Evgeniy <lnf_55@mail.ru>
2023-04-22 16:42:01 +0300

    добавил заготовки  обязательных задач по второму  семинару

--- homework/lesson2/task0.java
+++ homework/lesson2/task0.java
@@ -1,3 +1,8 @@
+// Посчитайте сколько драгоценных камней в куче обычных камней
+// Пример:
+// jewels = “aB”, stones = “aaaAbbbB”
+// Результат в консоль ”a3B1”
+
 package homework.lesson2;
 
 public class task0 {
