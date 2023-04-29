1b4fc40a2de28fcb48734d4a56c7d4290071c150
Evgeniy <lnf_55@mail.ru>
2023-04-29 16:40:00 +0300

    добавил решение задач к 4 уроку

--- homework/lesson4/task3.java
+++ homework/lesson4/task3.java
@@ -7,9 +7,9 @@ public class task3 {
     public static void main(String[] args) {
         LinkedList<Integer> list = new LinkedList<>();
         list.add(1);
-        list.add(3);
-        list.add(3);
-        list.add(3);
+        list.add(1);
+        list.add(1);
+        list.add(1);
 
         int sum = 0;
         Iterator<Integer> iterator = list.iterator();
