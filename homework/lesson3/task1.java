ab516b9e092b99d658631934b2f389bea4192ce8
Evgeniy <lnf_55@mail.ru>
2023-04-29 14:56:32 +0300

    добавил шаблоны к 3 заданию

---
+++ homework/lesson3/task1.java
@@ -0,0 +1,8 @@
+// Пусть дан произвольный список целых чисел, удалить из него четные числа
+package homework.lesson3;
+
+public class task1 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ homework/lesson3/task2.java
@@ -0,0 +1,9 @@
+// Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее арифметическое из этого списка. 
+// Collections.max()
+package homework.lesson3;
+
+public class task2 {
+    public static void main(String[] args) {
+        
+    }
+}
